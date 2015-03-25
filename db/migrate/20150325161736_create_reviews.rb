3429a0fb27ced24796e0acdf29ff4c6843ad8814
Justine <justinejones91@gmail.com>
2015-03-25 12:17:55 -0400

    added review model

---
+++ db/migrate/20150325161736_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :rating
+      t.text :comment
+      t.string :recommendation_level
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/reviews.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :review do
+    rating 1
+comment "MyText"
+recommendation_level "MyString"
+  end
+
+end
