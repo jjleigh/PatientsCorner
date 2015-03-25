489c9952cf1924dbe8ecf9f8b2cc4807b4023001
Justine <justinejones91@gmail.com>
2015-03-25 13:38:09 -0400

    migrated database

--- db/migrate/20150325162432_create_users.rb
+++ db/migrate/20150325162432_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.string :password_digest
       t.text :address_1
       t.text :address_2
       t.text :address_3
