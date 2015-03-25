0e33ac968c8d38b51d1dee8cdfd54e229b33cd32
Justine <justinejones91@gmail.com>
2015-03-25 12:38:01 -0400

    added patients and doctors

---
+++ spec/factories/doctors.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :doctor do
+    clinic_id 1
+category_id 1
+phone_number "MyString"
+url "MyString"
+photo_url "MyString"
+  end
+
+end
---
+++ spec/factories/patients.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :patient do
+    phone_number "MyString"
+photo_url "MyString"
+  end
+
+end
