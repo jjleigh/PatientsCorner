f9f71124499863bc005abf3d9aa98db59581d8e4
Justine <justinejones91@gmail.com>
2015-03-25 23:37:53 -0400

    added the categories controller

--- db/migrate/20150326031004_add_foreign_key_and_phone_number_to_clinic_table.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyAndPhoneNumberToClinicTable < ActiveRecord::Migration
-  def change
-  	add_column :clinics, :phone_number, :string
-  	add_column :clinics, :category_id, :string
-  end
-end
