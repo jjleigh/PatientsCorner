2088258defc3d87fb96f093d685086df5dd2f2c8
Justine <justinejones91@gmail.com>
2015-03-26 09:25:56 -0400

    added type column to the reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326131634) do
+ActiveRecord::Schema.define(version: 20150326132347) do
 
   create_table "appointments", force: :cascade do |t|
     t.text     "reason"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150326131634) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.integer  "reviewable_id"
+    t.string   "reviewable_type"
   end
 
   create_table "users", force: :cascade do |t|
